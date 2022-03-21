cef178333cd672ee48f77ab7e1c6ebc2b3ecebb4
Priscila Teodório da Silva <priscila.silva@seplag.al>
2022-03-21 12:16:02 -0300

    Teste

--- src/route.js
+++ src/route.js
@@ -6,6 +6,8 @@ route.get('/', (req, res)  => res.render("index"))
 route.get('/room', (req, res)  => res.render("room"))
 route.get('/create-room', (req, res)  => res.render("create-room"))
 
+//route.get('/room/:room/:question/:action', (req, res)  => res.render("exemplo", {req}))
+
 
 module.exports = route
 
