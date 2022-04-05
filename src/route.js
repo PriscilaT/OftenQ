c92b6b20aee1710d32d3d27a33b256b366481edd
priscilat <pts@ic.ufal.br>
2022-04-04 21:48:30 -0300

    Merge

--- src/route.js
+++ src/route.js
@@ -5,6 +5,9 @@ const route = express.Router()
 route.get('/', (req, res)  => res.render("index"))
 route.get('/room', (req, res)  => res.render("room"))
 route.get('/create-room', (req, res)  => res.render("create-room"))
+route.post('/room/:room/:question/:action', (req, res) => res.render())
+
+route.post()
 
 
 module.exports = route
